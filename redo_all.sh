cc791149dcf6f4458d2f231286f71da0319eb250
funderburkjim <funderburkjim@gmail.com>
2015-12-29 20:40:28 -0500

    recompute based on mw update

---
+++ redo_all.sh
@@ -0,0 +1,4 @@
+echo "redo step2"
+cd step2
+echo "ASSUMING YOU HAVE DONE redo_datasources.sh, or the equivalent"
+sh redo.sh
---
+++ redo_datasources.sh
@@ -0,0 +1,3 @@
+ cd ~/Documents/GitHub/MWderivations
+ cp ~/Documents/GitHub/MWlexnorm/step1b/lexnorm-all.txt step2/
+ cp ~/Documents/GitHub/MWvlex/step0/verb_step0a.txt step2/auxiliary/
