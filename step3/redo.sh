9dd22564cec043da32af2d92f6a81707fad0eab4
funderburkjim <funderburkjim@gmail.com>
2016-02-11 18:41:32 -0500

    recompute after MW update

--- step3/redo.sh
+++ step3/redo.sh
@@ -6,7 +6,7 @@ echo "Creating icf.txt from icf_prep1.txt"
 python init_icf.py auxiliary/icf_prep1.txt auxiliary/icf.txt
 echo "create mw_extract.txt from mw.xml"
 python mw_extract.py ../../mwxml/mw.xml auxiliary/mw_extract.txt
-echo "merge files into all.txt"ellison~1918
+echo "merge files into all.txt"
 #python merge.py auxiliary/mw_extract.txt allnom.txt auxiliary/verb_step0a.txt auxiliary/icf.txt all.txt > all_log.txt
 python merge.py auxiliary/mw_extract.txt allnom.txt ../../MWvlex/step0/verb_step0a.txt auxiliary/icf.txt all.txt > all_log.txt
 #sh redo_analysis.sh
