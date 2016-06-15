8608504896402b6e9641ff9d384ed79ac413af8c
funderburkjim <funderburkjim@gmail.com>
2016-06-15 17:03:05 -0400

    print summaries to step4 to redo_log.txt

--- redo_all.sh
+++ redo_all.sh
@@ -1,7 +1,13 @@
 echo "step2 now considered obsolete."
+echo "---------------------------------------"
 echo "recomputing step3"
+echo "---------------------------------------"
 cd step3
 #echo "ASSUMING YOU HAVE DONE redo_datasources.sh, or the equivalent"
 sh redo.sh
+echo "---------------------------------------"
+echo "recomputing step4"
+echo "---------------------------------------"
 cd ../step4
-sh redo.sh
+sh redo.sh > redo_log.txt
+
